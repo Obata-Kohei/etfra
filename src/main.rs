d63a26ada6dd4d672eb2adcb6ddc641df5e03d31
fg2fan <ocelot.12.ok@gmail.com>
2025-12-29 17:41:58 +0900

    20251229-01

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-use std::time::Instant;
-use chrono::{Local, DateTime};
-use num_complex::Complex;
+use eframe::egui;
 
-use etfra::prelude::*;
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "widget minimal",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp))),
+    )
+}
 
-fn main() {
-    let resolution: (usize, usize) = (2048 as usize, 2048 as usize);  // (w, h)
-    let center = Complex::new(-0.5, 0.);
-    let view_size = (3., 3.);
-    let max_iter = 500;
-    let escape_radius = 2.0;
-    //let mut palette = Palette::gradation_by_hue(256, 0., 360., 1.0, 1.0);
-    let mut palette = Palette::grayscale(256);
-    palette.reverse();
+struct MyApp;
 
-    let dynamics = Mandelbrot::new();
-    let escape = EscapeByCount::new(max_iter, escape_radius);
-    let coloring = PaletteColoring::new(palette, max_iter);
-
-    let frc = EscapeTimeFractal::new(dynamics, escape, coloring, resolution, center, view_size);
-
-    let start = Instant::now();
-    let img = frc.render_par();
-    let duration = start.elapsed();
-
-    println!("time elapsed: {:?}", duration);
-
-    let now: DateTime<Local> = Local::now();
-    let s = now.format("%Y%m%d%H%M%S").to_string();
-    img.save(format!("fractal_{s}.png")).expect("image should be saved");
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello egui");          // ラベル
+            ui.button("Button");             // ボタン
+        });
+    }
 }
---
+++ src/main2.rs
@@ -0,0 +1,58 @@
+use std::time::Instant;
+use chrono::{Local, DateTime};
+use num_complex::Complex;
+use eframe::egui;
+
+use etfra::prelude::*;
+/*
+fn main() -> eframe::Result<()> {
+    let resolution: (usize, usize) = (2048 as usize, 2048 as usize);  // (w, h)
+    let center = Complex::new(-0.5, 0.);
+    let view_size = (3., 3.);
+    let max_iter = 500;
+    let escape_radius = 2.0;
+    //let mut palette = Palette::gradation_by_hue(256, 0., 360., 1.0, 1.0);
+    let mut palette = Palette::grayscale(256);
+    palette.reverse();
+
+    let dynamics = Mandelbrot::new();
+    let escape = EscapeByCount::new(max_iter, escape_radius);
+    let coloring = PaletteColoring::new(palette, max_iter);
+
+    let frc = EscapeTimeFractal::new(dynamics, escape, coloring, resolution, center, view_size);
+
+    let start = Instant::now();
+    let img = frc.render_par();
+    let duration = start.elapsed();
+
+    println!("time elapsed: {:?}", duration);
+
+    let now: DateTime<Local> = Local::now();
+    let s = now.format("%Y%m%d%H%M%S").to_string();
+    //img.save(format!("fractal_{s}.png")).expect("image should be saved");
+
+
+    let options = eframe::MativeOptions::defaulr();
+    eframe::run_native(
+        "egui_sample",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default()))
+    )
+}
+
+#[derive(Default)]
+struct MyApp {
+    counter: i32,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello");
+            if ui.button("increment").clicked() {
+                self.counter += 1;
+            }
+            ui.label(format!("counter = {}", self.counter));
+        });
+    }
+} */
